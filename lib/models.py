4a1b8968ca9f5a71ce517a804d6ba4d20d65a939
kevin <kevin@novek.io>
2024-02-15 06:18:45 +0300

    feat: added Review model

--- lib/models.py
+++ lib/models.py
@@ -1,19 +1,25 @@
 import os
 import sys
-
-sys.path.append(os.getcwd)
-
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer)
-
+from sqlalchemy import create_engine, PrimaryKeyConstraint, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 engine = create_engine('sqlite:///db/restaurants.db', echo=True)
 
+class Review(Base):
+    __tablename__ = 'reviews'
 
-# class Review(Base):
-#     pass
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+    restaurant = relationship("Restaurant", back_populates="reviews")
+    customer = relationship("Customer", back_populates="reviews")
+
+    def __repr__(self):
+        return f'Review: {self.star_rating} stars'
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -21,6 +27,8 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String())
     price = Column(Integer)
+    
+    reviews = relationship("Review", back_populates="restaurant")
 
     def __repr__(self):
         return f'Restaurant: {self.name}'
@@ -32,5 +40,10 @@ class Customer(Base):
     first_name = Column(String())
     last_name = Column(String())
 
+    reviews = relationship("Review", back_populates="customer")
+
     def __repr__(self):
-        return f'Customer: {self.name}'
+        return f'Customer: {self.first_name} {self.last_name}'
+
+# Create tables
+Base.metadata.create_all(engine)
